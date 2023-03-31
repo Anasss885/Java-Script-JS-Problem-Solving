dfc1e1ce60fb8507421ee21c0bd4401a6e0080db
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-04-01 00:33:31 +0200

    Create Switch It Up.js

---
+++ Switch It Up.js
@@ -0,0 +1,22 @@
+function switchItUp(number){
+  //var a = number;
+let b = {
+0: "Zero",
+1: "One",
+2: "Two",
+3: "Three",
+4: "Four",
+5: "Five",
+6: "Six",
+7: "Seven",
+8: "Eight",
+9: "Nine"
+};
+return b[number];
+
+}
+//Another Sol
+function switchItUp(number){
+  var words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  return words[number];
+}

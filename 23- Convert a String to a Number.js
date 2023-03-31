01521f91459884bd2caec43c2f4ce756387d9eb7
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-03-31 02:46:41 +0200

    Create 23- Convert a String to a Number.js

---
+++ 23- Convert a String to a Number.js
@@ -0,0 +1,8 @@
+const stringToNumber = function(str){
+  return +str; // + operator converts from string to number 
+}
+// Another Sol
+const stringToNumber = function(str){
+  return Number(str);
+}
+

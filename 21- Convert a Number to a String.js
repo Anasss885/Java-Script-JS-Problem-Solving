7e157a1c20604ec8d90976fc428a43aad769b600
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-03-31 02:20:34 +0200

    Create 21- Convert a Number to a String.js

---
+++ 21- Convert a Number to a String.js
@@ -0,0 +1,10 @@
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString()
+}
+//Another Sol
+
+function numberToString(num) {
+ 
+  return `${num}`;  /// backtick is an operator that allow us to deal with strings in a differnt way , by inducing variabels that holds string value in the logic 
+}

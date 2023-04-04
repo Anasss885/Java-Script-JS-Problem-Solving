01ba4bc13fe4e93801ab04872787c436894dc67b
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-04-04 16:49:51 +0200

    Create 40- Opposites attract.js

---
+++ 40- Opposites attract.js
@@ -0,0 +1,9 @@
+function lovefunc(flower1, flower2){
+ if((flower1%2==0&&flower2%2!=0)||(flower1%2!=0&&flower2%2==0))
+   return true;
+  else return false;
+}
+//Another Sol
+function lovefunc(flower1, flower2){
+ return flower1%2!=flower2%2;
+}
